accbf42a3fc070ee6f9db93931d510688f7177cd
rlrlsigksigk@gmail.com <rlrlsigksigk@gmail.com>
2024-12-04 00:03:29 +0900

    Main()문의 공룡 따로 함수로 빼내기

--- main.c
+++ main.c
@@ -4,10 +4,16 @@
 
 void CursorView(char show);
 void SetConsoleView();
+void DrawDino();
 
 int main() {
 	SetConsoleView();
 	while (true) {
+
+		DrawDino();
+
+		Sleep(60);
+		system("cls");
 		static int legFlag = true;
 		printf("        $$$$$$$ \n");		// 8, 7, 1
 		printf("       $$ $$$$$$\n");		// 7, 2, 1, 6
@@ -37,6 +43,35 @@ int main() {
 
 		Sleep(60);
 		system("cls");
+	}
+}
+
+void DrawDino() {
+	static int legFlag = true;
+	printf("        $$$$$$$ \n");		// 8, 7, 1
+	printf("       $$ $$$$$$\n");		// 7, 2, 1, 6
+	printf("       $$$$$$$$$\n");		// 7, 9
+	printf("$      $$$      \n");		// 1, 6, 3, 6
+	printf("$$     $$$$$$$  \n");		// 2, 5, 7, 2
+	printf("$$$   $$$$$     \n");		// 3, 3, 5, 5
+	printf(" $$  $$$$$$$$$$ \n");		// 1, 2, 2, 10, 1
+	printf(" $$");									// 1, 2
+	printf("원광대");
+	printf("$$$    \n");						// 3, 4
+	printf("  $$$");								// 2, 3
+	printf("컴소공");
+	printf("$    \n");							// 1, 4
+	printf("    $$$$$$$$    \n");		// 4, 8, 4
+	printf("     $$$$$$     \n");		// 5, 6, 5
+	if (legFlag) {
+		printf("     $    $$$   \n");
+		printf("     $$         ");
+		legFlag = false;
+	}
+	else {
+		printf("     $$$  $     \n");
+		printf("          $$    ");
+		legFlag = true;
 	}
 }
 
